8e8afaeaf99c177aed29c574aa56dbe3982415dd
Richard Lindhout <richardlindhout96@gmail.com>
2019-12-17 12:30:24 +0100

    Added Selections for better viewing of what the user has selected

--- helper/preload.go
+++ helper/preload.go
@@ -38,7 +38,6 @@ func GetPreloadMods(ctx context.Context, preloadColumnMap map[string]ColumnSetti
 	gPreloads := GetPreloads(ctx)
 	for _, gPreload := range gPreloads {
 		dPreloadParts := []string{}
-		// fmt.Println("preloadje??", gPreload)
 		for _, gPreloadPart := range strings.Split(gPreload, ".") {
 			columnSetting, ok := preloadColumnMap[gPreloadPart]
 			if ok {
@@ -46,15 +45,10 @@ func GetPreloadMods(ctx context.Context, preloadColumnMap map[string]ColumnSetti
 					if PreloadsContainMoreThanId(gPreloads, gPreloadPart) {
 						dPreloadParts = append(dPreloadParts, columnSetting.Name)
 					}
-					// TODO
-					// dPreloadParts = append(dPreloadParts, columSetting.Name)
 				} else {
 					dPreloadParts = append(dPreloadParts, columnSetting.Name)
 				}
 			}
-			// else {
-			// 	fmt.Println(gPreload, " not found s")
-			// }
 		}
 		if len(dPreloadParts) > 0 {
 			queryMods = append(queryMods, qm.Load(strings.Join(dPreloadParts, ".")))
@@ -64,39 +58,19 @@ func GetPreloadMods(ctx context.Context, preloadColumnMap map[string]ColumnSetti
 }
 
 func GetPreloads(ctx context.Context) []string {
-	// return
-
 	return GetNestedPreloads(
 		graphql.GetRequestContext(ctx),
 		graphql.CollectFieldsCtx(ctx, nil),
 		"",
 	)
-	// arr2 := graphql.CollectAllFields(ctx)
-	// return append(arr1, arr2...)
 }
-
-// // CollectAllFields returns a slice of all GraphQL field names that were selected for the current resolver context.
-// // The slice will contain the unique set of all field names requested regardless of fragment type conditions.
-// func CollectAllFields(ctx context.Context) []string {
-// 	resctx := graphql.GetFieldContext(ctx)
-// 	collected := graphql.CollectFields(graphql.GetOperationContext(ctx), resctx.Field.Selections, nil)
-// 	uniq := make([]string, 0, len(collected))
-
-// 	for _, f := range collected {
-// 		fmt.Println("CollectAllFields add", f.Name)
-// 		for _, s := range f.Selections {
-
-// 		}
-// 		uniq = append(uniq, f.Name)
-// 	}
-// 	return uniq
-// }
 
 func GetNestedPreloads(ctx *graphql.RequestContext, fields []graphql.CollectedField, prefix string) (preloads []string) {
 	for _, column := range fields {
 		prefixColumn := GetPreloadString(prefix, column.Name)
 		preloads = append(preloads, prefixColumn)
 		preloads = append(preloads, GetNestedPreloads(ctx, graphql.CollectFields(ctx, column.SelectionSet, nil), prefixColumn)...)
+		preloads = append(preloads, GetNestedPreloads(ctx, graphql.CollectFields(ctx, column.Selections, nil), prefixColumn)...)
 	}
 	return
 }
