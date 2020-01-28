e73acda755580179216216461fd5e4151d1332ec
Richard Lindhout <richardlindhout96@gmail.com>
2020-01-28 22:44:58 +0100

    Added more fields like normal relationsships

--- boiler/sqlboiler.go
+++ boiler/sqlboiler.go
@@ -82,19 +82,46 @@ func ParseBoilerFile(dir string) (map[string]string, map[string]string) {
 
 					for _, field := range safeStructDecl.Fields.List {
 						// if t := reflect.TypeOf(field.Type); t.Kind() == reflect.Ptr {
-						// 	fmt.Println("STRUCT?????", "*"+t.Elem().Name())
+						// 	fmt.Println("TYPE OF STRUCT", "*"+t.Elem().Name())
 						// 	// return
-						// } else {
+						// }
+						//else {
 						// 	fmt.Println("STRUCT?????", t.Name())
 						// 	// return
 						// }
-						// fmt.Println("STRUCT?????", field.Type)
-						// fmt.Println(field.Names[0], field.Type)
-						switch field.Type.(type) {
+						// fmt.Println("STRUCT?????", field.Names, "  ", field.Type)
+						// fmt.Println(field.Names, field.Type)
+						switch xv := field.Type.(type) {
+
+						// case *ast.StructType:
+						// 	// t, _ := field.Type.(*ast.StructType)
+						// 	name := field.Names[0].Name
+						// 	fmt.Println("NOT IMPLMENTED", name)
+
+						case *ast.StarExpr:
+
+							if len(field.Names) > 0 {
+								name := field.Names[0].Name
+								// fmt.Println("NOT IMPLEMENTED", name)
+								// t1, _ := field.Type.(*ast.StarExpr)
+								// t2, _ := t1.Star.(*ast.Star)
+								// fmt.Println("type??", t2)
+								// field = v
+								if si, ok := xv.X.(*ast.Ident); ok {
+
+									key := safeTypeSpec.Name.String() + "." + name
+									//https://stackoverflow.com/questions/28246970/how-to-parse-a-method-declaration
+									fieldsMap[key] = si.Name
+								}
+
+							} else {
+								// fmt.Println("len(field.Names) == 0", field)
+							}
 
 						case *ast.SelectorExpr:
 							t, _ := field.Type.(*ast.SelectorExpr)
 							name := field.Names[0].Name
+							// fmt.Println("YESS above", name)
 							// if strings.HasPrefix(name, "_") || strings.HasPrefix(structName, "_") {
 							// 	continue
 							// }
@@ -106,9 +133,11 @@ func ParseBoilerFile(dir string) (map[string]string, map[string]string) {
 						// case *ast.ArrayType:
 
 						case *ast.Ident:
+
 							t, _ := field.Type.(*ast.Ident) // The type as a string
 							typeName := t.Name
 							name := field.Names[0].Name //name as a string
+							// fmt.Println("YESS ident", name)
 							// if strings.HasPrefix(name, "_") || strings.HasPrefix(structName, "_") {
 							// 	continue
 							// }
