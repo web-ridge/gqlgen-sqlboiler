b2c83096785d2a459f4d0c83e6fb86a6fb6b3539
Richard Lindhout <richardlindhout96@gmail.com>
2020-04-30 22:14:39 +0200

    Fix for getting nested input maps

--- helper/input.go
+++ helper/input.go
@@ -3,15 +3,23 @@ package helper
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/99designs/gqlgen/graphql"
 )
 
 func GetInputFromContext(ctx context.Context, key string) map[string]interface{} {
-	requestContext := graphql.GetRequestContext(ctx)
-	m, ok := requestContext.Variables[key].(map[string]interface{})
-	if !ok {
-		fmt.Println("can not get input from context")
+	requestContext := graphql.GetOperationContext(ctx)
+	variables := requestContext.Variables
+
+	// pick nested inputs
+	var ok bool
+	for _, splittedKey := range strings.Split(key, ".") {
+		variables, ok = variables[splittedKey].(map[string]interface{})
+		if !ok {
+			fmt.Println("can not get input from context for key: "+key+" splitted key: ", splittedKey)
+		}
 	}
-	return m
+
+	return variables
 }
